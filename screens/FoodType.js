ed3b5e6696ca11994f92f3742c5df33ad42dc343
Autumn Smith <autumn07@gmail.com>
2017-08-29 21:38:58 -0700

    ES6 changes / exporting on declaration
    Using an arrow function instead of binding to this in the constructor. Destructuring / export at definition / no need for else, just return

--- screens/FoodType.js
+++ screens/FoodType.js
@@ -5,34 +5,34 @@ import styles from '../styles/styles';
 import FoodTypeCollection from '../components/FoodTypeCollection';
 import FoodTypeResult from '../components/FoodTypeResult';
 
-class FoodType extends Component {
+export default class FoodType extends Component {
   constructor() {
     super();
     this.state = {
       chosenFood: ''
     };
-    this.chooseFood = this.chooseFood.bind(this);
   }
+
   static navigationOptions = {
     title: 'Random Food Type'
   }
 
-  chooseFood(chosenFood) {
+  chooseFood = (chosenFood) => (
     this.setState({
       chosenFood
     });
-  }
+  )
 
   getScreenContent() {
-    if (this.state.chosenFood === '') {
+    const { chosenFood } = this.state;
+    if (chosenFood === '') {
       return (
         <FoodTypeCollection selectFood={this.chooseFood}/>
       );
-    } else {
-      return (
-        <FoodTypeResult selectedFood={this.state.chosenFood} selectFood={this.chooseFood}/>
-      );
     }
+    return (
+      <FoodTypeResult selectedFood={chosenFood} selectFood={this.chooseFood}/>
+    );
   }
 
   render() {
@@ -43,5 +43,3 @@ class FoodType extends Component {
     );
   }
 }
-
-export default FoodType;
