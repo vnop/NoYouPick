0521024fe1873960600cb00e4cc30f6147048e97
Vandeth Nop <vnop14@gmail.com>
2017-09-03 19:28:38 -0700

    Restaurant collection styling complete
    Layout of RestaurantCollection component is complete. Added ScrollView and centered button text.

--- components/RestaurantCollection.js
+++ components/RestaurantCollection.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Image, Text, View, TouchableHighlight } from 'react-native';
+import {
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import PropTypes from 'prop-types';
 import styles from '../styles/styles';
@@ -30,37 +38,37 @@ class RestaurantCollection extends Component {
           </TouchableHighlight>
         </View>
 
-        <View style={tempStyles.foodGrid}>
-          { this.props.restaurants.map((place, index) => {
-            let imageUri = `${place.venue.featuredPhotos.items[0].prefix}200${place.venue.featuredPhotos.items[0].suffix}`;
-            return (
-              <Image
-                source={{uri: imageUri}}
-                style={{ width: imageWidth, height: 150 }}
-                key={place.venue.featuredPhotos.items[0].id}
-              >
-              <View style={{ backgroundColor: 'rgba(0,0,0,0.4)', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 15,
-                    fontWeight: 'bold'
-                  }}
-                >
-                  {place.venue.name}
-                </Text>
-              </View>
-              </Image>
-            );
-          }) }
-        </View>
+        <ScrollView>
+          <View style={tempStyles.foodGrid}>
+              { this.props.restaurants.map((place, index) => {
+                let imageUri = `${place.venue.featuredPhotos.items[0].prefix}200${place.venue.featuredPhotos.items[0].suffix}`;
+                return (
+                  <Image
+                    source={{uri: imageUri}}
+                    style={{ width: imageWidth, height: 150 }}
+                    key={place.venue.featuredPhotos.items[0].id}
+                  >
+                  <View style={{ backgroundColor: 'rgba(0,0,0,0.4)', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text
+                      style={{
+                        color: 'white',
+                        fontSize: 15,
+                        fontWeight: 'bold'
+                      }}
+                    >
+                      {place.venue.name}
+                    </Text>
+                  </View>
+                  </Image>
+                );
+              }) }
+          </View>
+        </ScrollView>
 
       </View>
     );
   }
 }
-
-export default RestaurantCollection;
 
 const tempStyles = StyleSheet.create({
   container: {
@@ -68,12 +76,12 @@ const tempStyles = StyleSheet.create({
   },
   foodGrid: {
     flex: 1,
-    backgroundColor: 'skyblue',  // to be removed
     flexDirection: 'row',
     flexWrap: 'wrap',
-    // justifyContent: 'center'
   },
   foodButton: {
     marginBottom: 1,
   }
 });
+
+export default RestaurantCollection;
--- styles/styles.js
+++ styles/styles.js
@@ -11,6 +11,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'royalblue',
     padding: 20,
     borderRadius: 5,
+    alignItems: 'center',
   },
   disabledBtn: {
     backgroundColor: 'grey',
